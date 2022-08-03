62d940dc4c00c727bacaaf392a559bc58dd12ab3
MKVar <65726773+MKVar@users.noreply.github.com>
2022-08-03 21:21:44 +0530

    Add files via upload

---
+++ src/assets/lottie/index.js
@@ -0,0 +1,7 @@
+export ANIMATION_LOADING from "./loading";
+export ANIMATION_EMPTY_BOX from "./empty_box";
+export ANIMATION_EMPTY_FOLDER from "./empty_folder";
+export ANIMATION_TWO_ARROW_DOWN from "./two_arrow_down";
+export ANIMATION_CONSTRUCTION from "./construction";
+export ANIMATION_HELLO from "./hello";
+export ANIMATION_STRESSED from "./stressed";
